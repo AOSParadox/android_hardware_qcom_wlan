0db1896f17658cf4555c3cebbb4daace59479301
Steve Kondik <shade@chemlab.org>
2014-11-10 20:21:54 -0800

    wcnss-service: Additional guard on QMI support
    Change-Id: Ib03062ec7713f93b208dcd0650921e713abe169f
    Signed-off-by: theh2o64@gmail.com <theh2o64@gmail.com>
    Signed-off-by: Louis Popi <theh2o64@gmail.com>

--- wcnss-service/Android.mk
+++ wcnss-service/Android.mk
@@ -4,6 +4,8 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := wcnss_service
 LOCAL_C_INCLUDES += $(TARGET_OUT_HEADERS)/common/inc/
 LOCAL_SRC_FILES := wcnss_service.c
+LOCAL_SHARED_LIBRARIES := libc libcutils libutils liblog
+ifneq ($(QCPATH),)
 ifeq ($(strip $(TARGET_USES_QCOM_WCNSS_QMI)),true)
 LOCAL_CFLAGS += -DWCNSS_QMI
 LOCAL_C_INCLUDES += $(TARGET_OUT_HEADERS)/qmi-framework/inc
@@ -11,13 +13,11 @@ LOCAL_C_INCLUDES += $(TARGET_OUT_HEADERS)/qmi/services
 LOCAL_C_INCLUDES += $(TARGET_OUT_HEADERS)/qmi/platform
 LOCAL_C_INCLUDES += $(TARGET_OUT_HEADERS)/qmi/inc
 LOCAL_SRC_FILES += wcnss_qmi_client.c
-endif #TARGET_USES_QCOM_WCNSS_QMI
-LOCAL_SHARED_LIBRARIES := libc libcutils libutils liblog
-ifeq ($(strip $(TARGET_USES_QCOM_WCNSS_QMI)),true)
 LOCAL_SHARED_LIBRARIES += libqmiservices libqmi_cci
 LOCAL_C_INCLUDES += $(TARGET_OUT_HEADERS)/libmdmdetect/inc
 LOCAL_SHARED_LIBRARIES += libmdmdetect
 endif #TARGET_USES_QCOM_WCNSS_QMI
+endif #QCPATH
 LOCAL_CFLAGS += -DDYNAMIC_NV
 LOCAL_MODULE_TAGS := optional
 LOCAL_CFLAGS += -Wall -Werror
