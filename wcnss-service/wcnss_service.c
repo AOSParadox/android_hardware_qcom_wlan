a719939c0ac56f1618a81bd7fe7f47e2f46f4f1e
Arne Coucheron <arco68@gmail.com>
2016-09-02 20:43:01 +0200

    wcnss_service: Fix build fail when WCNSS_QMI_OSS is defined
    Change-Id: I9b14f384901586df5c5e255b15cbeeba20bb6277
    Signed-off-by: theh2o64@gmail.com <theh2o64@gmail.com>
    Signed-off-by: Louis Popi <theh2o64@gmail.com>

--- wcnss-service/wcnss_service.c
+++ wcnss-service/wcnss_service.c
@@ -400,7 +400,7 @@ unsigned int convert_string_to_hex(char* string)
 }
 
 
-#ifdef WCNSS_QMI
+#if defined(WCNSS_QMI) || defined(WCNSS_QMI_OSS)
 void setup_wcnss_parameters(int *cal, int nv_mac_addr)
 #else
 void setup_wcnss_parameters(int *cal)
@@ -790,7 +790,7 @@ int main(int argc, char *argv[])
 	UNUSED(argc), UNUSED(argv);
 	int rc;
 	int fd_dev, ret_cal;
-#ifdef WCNSS_QMI
+#if defined(WCNSS_QMI) || defined(WCNSS_QMI_OSS)
 	int nv_mac_addr = FAILED;
 #ifdef MDM_DETECT
 	struct dev_info mdm_detect_info;
@@ -871,7 +871,7 @@ nomodem:
 	dynamic_nv_replace();
 #endif
 
-#ifdef WCNSS_QMI
+#if defined(WCNSS_QMI) || defined(WCNSS_QMI_OSS)
 	setup_wcnss_parameters(&ret_cal, nv_mac_addr);
 #else
 	setup_wcnss_parameters(&ret_cal);
